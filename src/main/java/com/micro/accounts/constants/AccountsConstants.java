563dca270eec9638c837d2c71cff9ecd3dd2c90e
Subelal PAL <subelalrpal@gmail.com>
2024-07-17 14:40:36 +0530

    Modifier exchanges in AccountsConstants.java

--- src/main/java/com/micro/accounts/constants/AccountsConstants.java
+++ src/main/java/com/micro/accounts/constants/AccountsConstants.java
@@ -5,16 +5,16 @@ public class AccountsConstants {
     private AccountsConstants(){
 
     }
-    public final static String SAVINGS = "Savings";
-    public final static String ADDRESS = "123 Megapolish Pune India";
+    public static final  String SAVINGS = "Savings";
+    public static final  String ADDRESS = "123 Megapolish Pune India";
 
-    public final static String STATUS_201 = "201";
-    public final static String MESSAGE_201 ="Accounts Created Successfully";
+    public static final  String STATUS_201 = "201";
+    public static final  String MESSAGE_201 ="Accounts Created Successfully";
 
-    public final static String STATUS_200 = "200";
-    public final static String MESSAGE_200 ="Request Process Successfully";
+    public static final  String STATUS_200 = "200";
+    public static final  String MESSAGE_200 ="Request Process Successfully";
 
-    public final static String STATUS_500 = "500";
-    public final static String MESSAGE_500 ="An error occurred. Please try again and Contact Dev Team ";
+    public static final  String STATUS_500 = "500";
+    public static final String MESSAGE_500 ="An error occurred. Please try again and Contact Dev Team ";
 
 }
